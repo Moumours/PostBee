3d4853bfb94b989bb22b6b2115122bab9a933578
IDalb <synfoniz@gmail.com>
2023-06-06 18:26:42 +0200

    - Recréation de l'Activity "Home" suite à une erreur - Ajout d'un titre et d'une entrée de texte sur la MainActivity - Ajout d'un bouton pour aller de la MainActivity à la HomeActivity

--- mobile_app/app/src/main/java/com/example/mobile_app/controller/MainActivity.java
+++ mobile_app/app/src/main/java/com/example/mobile_app/controller/MainActivity.java
@@ -2,15 +2,30 @@ package com.example.mobile_app.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mobile_app.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button homeButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeButton = findViewById(R.id.main_button_home);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeActivityIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeActivityIntent);
+            }
+        });
     }
 }
