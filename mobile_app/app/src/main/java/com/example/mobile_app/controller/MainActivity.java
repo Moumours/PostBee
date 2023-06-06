675a42746d6bac394384281d5f2fee0aaaf80af4
mariaaitbouserhane <maria.ait-bouserhane@uha.fr>
2023-06-07 00:24:28 +0200

    interface Version1

--- mobile_app/app/src/main/java/com/example/mobile_app/controller/MainActivity.java
+++ mobile_app/app/src/main/java/com/example/mobile_app/controller/MainActivity.java
@@ -1,30 +1,49 @@
 package com.example.mobile_app.controller;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mobile_app.R;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button homeButton;
-
+    private EditText nom, prenom, email, password, confirmPassword;
+    private RadioGroup statusRadioGroup;
+    private Button inscriptionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        homeButton = findViewById(R.id.main_button_home);
+        nom = findViewById(R.id.main_edittext_nom);
+        prenom = findViewById(R.id.main_edittext_prenom);
+        email = findViewById(R.id.main_edittext_email);
+        password = findViewById(R.id.main_edittext_password);
+        confirmPassword = findViewById(R.id.main_edittext_confirmPassword);
+        statusRadioGroup = findViewById(R.id.main_edittext_statusRadioGroup);
+        inscriptionButton = findViewById(R.id.main_edittext_inscriptionButton);
 
-        homeButton.setOnClickListener(new View.OnClickListener() {
+        inscriptionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent homeActivityIntent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(homeActivityIntent);
+                String nomText = nom.getText().toString();
+                String prenomText = prenom.getText().toString();
+                String emailText = email.getText().toString();
+                String passwordText = password.getText().toString();
+                String confirmPasswordText = confirmPassword.getText().toString();
+
+                int selectedId = statusRadioGroup.getCheckedRadioButtonId();
+                RadioButton selectedRadioButton = findViewById(selectedId);
+                String statut = selectedRadioButton.getText().toString();
+
+
+                Toast.makeText(MainActivity.this, "Inscription réussie!", Toast.LENGTH_SHORT).show();
             }
         });
     }
