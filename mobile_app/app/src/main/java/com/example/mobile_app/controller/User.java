52050c801a5e4809fe5bc1ac1897972f984de834
Ayhan <ayhan.durukan25@gmail.com>
2023-06-07 08:58:36 +0200

    Ajout de la classe User

---
+++ mobile_app/app/src/main/java/com/example/mobile_app/controller/User.java
@@ -0,0 +1,22 @@
+package com.example.mobile_app.controller;
+
+public class User {
+
+    public User(String firstName, String lastName, String email, String password1,String password2,int ensisaGroup) {
+        this.first_name = firstName;
+        this.last_name = lastName;
+        this.email = email;
+        this.password1 = password1;
+        this.password2 = password2;
+        this.ensisaGroup = ensisaGroup;
+    }
+
+    private int ensisaGroup;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String password1;
+    private String password2;
+
+}
+
