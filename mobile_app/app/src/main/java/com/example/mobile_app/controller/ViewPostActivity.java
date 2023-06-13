d369e5eacc9c1828e1891a7cd12a51c465860ee8
rimxmahrez <rimben68@gmail.com>
2023-06-13 19:00:31 +0200

    j'espère  que mes petites fct vous font plaisir isaac dsl pour la classe comment j'ai mis quelques trucs dont j'avais besoin :)

--- mobile_app/app/src/main/java/com/example/mobile_app/controller/ViewPostActivity.java
+++ mobile_app/app/src/main/java/com/example/mobile_app/controller/ViewPostActivity.java
@@ -3,22 +3,28 @@ package com.example.mobile_app.controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.mobile_app.R;
+import com.example.mobile_app.model.Comment;
 import com.example.mobile_app.model.Token;
 import com.example.mobile_app.model.ViewPost;
 import com.google.gson.Gson;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewPostActivity extends AppCompatActivity {
 
@@ -41,7 +47,7 @@ public class ViewPostActivity extends AppCompatActivity {
         mTextContent = findViewById(R.id.viewpost_textview_content);
 
         Intent i = getIntent();
-        int postId = i.getIntExtra("ID",0);
+        int postId = i.getIntExtra("ID", 0);
         downloadViewPost(postId);
 
         mTextTitle.setText(i.getStringExtra("TITLE"));
@@ -50,28 +56,120 @@ public class ViewPostActivity extends AppCompatActivity {
 
         mTokenAccess = i.getStringExtra("TOKEN_ACCESS");
 
-        Log.d("ViewPostActivity","Voici l'id : " + postId);
+        Log.d("ViewPostActivity", "Voici l'id : " + postId);
+
+        // Déclenche le chargement des données du post et des commentaires
+        loadPostAndComments("http://postbee.alwaysdata.net/post/?id=" + postId);
 
         // ViewPost a aussi les documents et les commentaires
         // ViewPost.getListdocument()
         // ViewPost.getListcomment()
+
+        // RIM
     }
+
+    private static class FetchPostTask extends AsyncTask<String, Void, Void> {
+
+        private TextView mTextContent;
+
+        private String postText;
+        private List<Comment> comments;
+
+        public FetchPostTask(TextView textContent) {
+            mTextContent = textContent;
+        }
+
+        @Override
+        protected Void doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // Lire la réponse de l'API
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    reader.close();
+
+                    // Analyser la réponse pour extraire le texte du post et les commentaires
+                    parsePostAndComments(response.toString());
+                } else {
+                    Log.e("ViewPostActivity", "Erreur de requête HTTP: " + responseCode);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            // Le code de votre méthode onPostExecute ici
+            mTextContent.setText(postText);
+            // Utiliser les commentaires pour mettre à jour votre vue des commentaires
+            // Par exemple, vous pouvez les ajouter à une liste ou les afficher dans un RecyclerView
+            for (Comment comment : comments) {
+                // Faites quelque chose avec chaque commentaire
+                // comment.getText()
+                // comment.getAuthor()
+                // comment.getFullName()
+                // comment.getProfilePicture()
+                // comment.getDate()
+            }
+        }
+
+        private void parsePostAndComments(String response) throws JSONException {
+            // Convertir la réponse en un objet JSON
+            JSONObject jsonData = new JSONObject(response);
+            // Extraire le texte du post
+            postText = jsonData.getString("post_text");
+            // Extraire la liste de commentaires
+            JSONArray jsonComments = jsonData.getJSONArray("comments");
+            comments = new ArrayList<>();
+
+            // Parcourir chaque commentaire dans la liste
+            for (int i = 0; i < jsonComments.length(); i++) {
+                JSONObject jsonComment = jsonComments.getJSONObject(i);
+                // Extraire les détails du commentaire
+                String commentText = jsonComment.getString("comment_text");
+                String author = jsonComment.getString("author");
+                String fullName = jsonComment.getString("full_name");
+                String profilePicture = jsonComment.getString("profile_picture");
+                String date = jsonComment.getString("date");
+                // Créer un objet Comment et l'ajouter à la liste des commentaires
+                Comment comment = new Comment(commentText, author, fullName, profilePicture, date);
+                comments.add(comment);
+            }
+        }
+    }
+
+    public void loadPostAndComments(String apiUrl) {
+        FetchPostTask task = new FetchPostTask(mTextContent);
+        task.execute(apiUrl);
+    }
+
 
     public void downloadViewPost(int postId) {
         new Thread(new Runnable() {
             public void run() {
                 try {
                     URL url = new URL("http://postbee.alwaysdata.net/post/?id=" + postId);
-                    mViewPost = (ViewPost.class).cast(Token.connectToServer("post/?id=" + postId,"GET",mTokenAccess,null,null, Token.class,null));
+                    mViewPost = (ViewPost.class).cast(Token.connectToServer("post/?id=" + postId, "GET", mTokenAccess, null, null, Token.class, null));
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            if(mViewPost != null) {
-                                Log.d("ViewPostActivity","Post content received successfully");
+                            if (mViewPost != null) {
+                                Log.d("ViewPostActivity", "Post content received successfully");
                                 mTextContent.setText(mViewPost.getText());
-                            }
-                            else{
-                                Log.d("ViewPostActivity","Post content not received");
+                            } else {
+                                Log.d("ViewPostActivity", "Post content not received");
                             }
                         }
                     });
@@ -82,4 +180,3 @@ public class ViewPostActivity extends AppCompatActivity {
         }).start();
     }
 }
-
--- mobile_app/app/src/main/java/com/example/mobile_app/model/Comment.java
+++ mobile_app/app/src/main/java/com/example/mobile_app/model/Comment.java
@@ -1,4 +1,18 @@
 package com.example.mobile_app.model;
 
 public class Comment {
+    private String commentText;
+    private String author;
+    private String fullName;
+    private String profilePicture;
+    private String date;
+
+    public Comment(String commentText, String author, String fullName, String profilePicture, String date) {
+        this.commentText = commentText;
+        this.author = author;
+        this.fullName = fullName;
+        this.profilePicture = profilePicture;
+        this.date = date;
+    }
+
 }
