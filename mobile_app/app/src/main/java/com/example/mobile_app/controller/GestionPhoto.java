9bd95c25b8e7e49b3bee5686ca4ac69de6fe82a9
Pierre Albertini <pierrealbertini@icloud.com>
2023-06-13 10:00:25 +0200

    Ajout de la gestion des photos

---
+++ mobile_app/app/src/main/java/com/example/mobile_app/controller/GestionPhoto.java
@@ -0,0 +1,36 @@
+package com.example.mobile_app.controller;
+
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.provider.MediaStore;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class GestionPhoto {
+
+    private static final int PERMISSION_REQUEST_CODE = 123;
+    private static final int REQUEST_IMAGE_PICK = 456;
+
+    private boolean checkPermission(Activity activity) {
+        int result = ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission(Activity activity) {
+        ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+
+    public void selectPhoto(Activity activity) {
+        if (checkPermission(activity)) {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.setType("image/*");
+            activity.startActivityForResult(intent, REQUEST_IMAGE_PICK);
+        } else {
+            requestPermission(activity);
+        }
+    }
+}
