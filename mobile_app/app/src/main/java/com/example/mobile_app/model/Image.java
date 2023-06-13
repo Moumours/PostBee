b8f657dbc7ea5ef682da66591cab1a9a1f9beb07
Pierre Albertini <pierrealbertini@icloud.com>
2023-06-13 11:23:02 +0200

    Ajout premiere version Image et Video

---
+++ mobile_app/app/src/main/java/com/example/mobile_app/model/Image.java
@@ -0,0 +1,60 @@
+package com.example.mobile_app.model;
+
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.provider.MediaStore;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class Image extends Document {
+    private int width;
+    private int height;
+
+    private static final int PERMISSION_REQUEST_CODE = 123;
+    private static final int REQUEST_IMAGE_PICK = 456;
+
+    public Image(String name, String fileType, String filePath, int width, int height) {
+        super(name, fileType, filePath);
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    private boolean checkPermission(Activity activity) {
+        int result = ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission(Activity activity) {
+        ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+
+    public void selectPhoto(Activity activity) {
+        if (checkPermission(activity)) {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.setType("image/*");
+            activity.startActivityForResult(intent, REQUEST_IMAGE_PICK);
+        } else {
+            requestPermission(activity);
+        }
+    }
+}
