32bcd1104eabc9ba08caf7cc9ed9cfdbde266de2
AlexandreCaux <alexandre.caux@live.fr>
2023-06-14 15:22:33 +0200

    Suppression des boutons de Zoé pour rendre l'app fonctionnelle

--- mobile_app/app/src/main/java/com/example/mobile_app/controller/EditPostActivity.java
+++ mobile_app/app/src/main/java/com/example/mobile_app/controller/EditPostActivity.java
@@ -57,7 +57,7 @@ public class EditPostActivity extends AppCompatActivity {
 
         mEditTextTitle = findViewById(R.id.editpost_edittext_title);
         mEditTextContent = findViewById(R.id.editpost_edittext_content);
-        mButtonImage = findViewById(R.id.edit_post_button_photo);
+        mButtonImage = findViewById(R.id.editpost_button_picture);
         mButtonImage.setOnClickListener(v -> GestionPhoto.selectPhoto(this));
         mButtonSubmit = findViewById(R.id.editpost_button_submit);
 
