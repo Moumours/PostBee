f650f3954f61c7bbd56905a72dd379530040d94b
Ayhan <ayhan.durukan25@gmail.com>
2023-06-15 10:51:45 +0200

    La moderation est optiniser AU MAXXXXXXXXXXXX

--- mobile_app/app/src/main/java/com/example/mobile_app/model/item_post/ItemPostValidationViewHolder.java
+++ mobile_app/app/src/main/java/com/example/mobile_app/model/item_post/ItemPostValidationViewHolder.java
@@ -7,15 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.mobile_app.R;
 import com.example.mobile_app.model.Token;
 import com.example.mobile_app.model.UserStatic;
 import com.example.mobile_app.model.RecyclerViewInterface;
-
 import android.os.Handler;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -29,7 +26,7 @@ public class ItemPostValidationViewHolder extends RecyclerView.ViewHolder {
 
     public ItemPostValidationViewHolder(@NonNull View itemView, RecyclerViewInterface recyclerViewInterface, ItemPostValidationAdapter adapter) {
         super(itemView);
-        this.adapter = adapter; // Initialization of adapter
+        this.adapter = adapter;
 
 
         text_title = itemView.findViewById(R.id.itempost_textview_title);
@@ -44,7 +41,7 @@ public class ItemPostValidationViewHolder extends RecyclerView.ViewHolder {
         accept_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int position = getAdapterPosition(); // Get position before network call
+                int position = getAdapterPosition();
                 postDecision.setPostId(postId);
                 postDecision.setResponse("true");
                 sendPostDecision(postDecision, adapter, position);
@@ -55,7 +52,7 @@ public class ItemPostValidationViewHolder extends RecyclerView.ViewHolder {
         deny_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int position = getAdapterPosition(); // Get position before network call
+                int position = getAdapterPosition();
                 postDecision.setPostId(postId);
                 postDecision.setResponse("false");
                 sendPostDecision(postDecision, adapter, position);
@@ -85,13 +82,12 @@ public class ItemPostValidationViewHolder extends RecyclerView.ViewHolder {
                     String endUrl = "approve";
                     Token.connectToServer(endUrl,"POST",UserStatic.getAccess(),postDecision,PostDecision.class,null, null);
 
-                    // Run on UI thread
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
                             if (position != RecyclerView.NO_POSITION) {
                                 adapter.removeItem(position);
-                                adapter.notifyDataSetChanged(); // Notify RecyclerView about the change in data
+                                adapter.notifyDataSetChanged();
                             }
                         }
                     });
